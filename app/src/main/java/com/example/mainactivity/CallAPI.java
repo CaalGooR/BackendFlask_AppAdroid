68069e887c2f312dae9b28da18e5b5f4f47f8eae
Carlos Alejandro Gonzalez Ortiz <carlos.alejandro.gonzalez.ortiz@uabc.edu.mx>
2020-02-05 10:43:00 -0800

    Base Project

---
+++ app/src/main/java/com/example/mainactivity/CallAPI.java
@@ -0,0 +1,17 @@
+package com.example.mainactivity;
+
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class CallAPI extends AsyncTask<String,String,String>
+{
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
